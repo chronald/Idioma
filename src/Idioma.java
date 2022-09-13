8e7920ad2a785ce14e75db36677bef028ea98415
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-09-13 15:28:27 -0300

    atualizando

--- src/Idioma.java
+++ src/Idioma.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Idioma {
+import java.util.Locale;
+
+public class Idioma {
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println("O idioma do seu sistema está em : ");
+        System.out.println(loc.getDisplayLanguage());
+        System.out.println(loc.getLanguage());
+    }
+
 }
